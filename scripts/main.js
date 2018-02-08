5cbfb6fdc52b43845fc34bcecf1d6bdfbdabf074
moitshela <36259146+moitshela@users.noreply.github.com>
2018-02-08 11:11:29 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/lucas.JPG')
+     {
+      myImage.setAttribute ('src','images/lucas2.JPG');
+    } else {
+      myImage.setAttribute ('src','images/lucas.JPG');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'PROGRAMING IS COOL, ' + storedName;
+}
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'PROGRAMING IS COOL, ' + myName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
